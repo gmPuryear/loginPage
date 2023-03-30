70e138906766ec69679d548996236c066e78b041
George Puryear <gm.puryear@yahoo.com>
2023-03-30 08:18:33 -0500

    started to implement react-auth-kit library for login

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import store from './store';
 import {Provider} from 'react-redux';
+import { AuthProvider } from 'react-auth-kit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <AuthProvider
+        authType={'cookie'}
+        authName={'_auth'}
+        cookieDomain={'window.location.hostname'}
+        cookieSecure={false}
+        />
+
         {/*"BrowserRouter" provides information in routing to all components to make everything work*/}
         <BrowserRouter>
             <Provider store={store}>
