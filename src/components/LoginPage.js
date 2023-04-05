3d3aabbb5a8e83b058253f553e7be7fca488c8da
George Puryear <gm.puryear@yahoo.com>
2023-04-05 12:18:30 -0500

    added password type to login password and padding to register here button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,23 +1,36 @@
 import React, {useEffect, useState, useContext, useReducer} from 'react';
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {v4 as uuid} from 'uuid';
 import RegisterModal from './RegisterModal';
 import {useForm} from 'react-hook-form';
 import RegisterModalContext from './RegisterModalContext';
 import axios from 'axios';
-import { useSelector } from 'react-redux';
-import { useSignIn } from 'react-auth-kit';
+import {useSelector} from 'react-redux';
+import {useSignIn} from 'react-auth-kit';
 
 const LoginPage = () => {
 
     const registerModalContext = useContext(RegisterModalContext);
 
-    const {register, handleSubmit} = useForm();
+    const {
+        register,
+        handleSubmit,
+        setError,
+        reset,
+        formState,
+        formState: { 
+            errors,
+            isSubmitSuccessful
+        }
+    } = useForm();
+
+    useEffect(() => {
+        if (formState.isSubmitSuccessful) {
+            reset()
+        }
+    })
 
     const navigate = useNavigate();
-
-    // const auth = useSelector((state) => state.auth)
-    // console.log(auth);
 
     const signIn = useSignIn(); // takes care of all authentication
 
@@ -26,10 +39,11 @@ const LoginPage = () => {
         <div className="login_card">
             <h3 className="login_title">Login</h3>
 
-            <form className="login_form" onSubmit={handleSubmit(async (loginUserData) => {
+            <form className="login_form" onSubmit={
+                handleSubmit(async (loginUserData) => {
 
                 const {
-                    email,
+                    email, 
                     password
                 } = loginUserData;
 
@@ -43,48 +57,52 @@ const LoginPage = () => {
 
 
                 try {
-                    
+
                     const res = await axios.post('http://localhost:6060/api/auth', body, config);
-                    console.log(res.data.token);   
 
                     if (res.status === 200) {
-                        console.log(loginUserData);
                         signIn({
                             token: res.data.token,
                             expiresIn: .5,
                             tokenType: "Bearer",
                             authState: {email: loginUserData.email}
-                })
-                navigate('/home');
+                        })
+                        navigate('/home');
                     }
-                    
-
 
                 } catch (err) {
-                        console.log(err);
-
-                    }
-                    // USE AUTH ROUTE FOR LOGIN. CHECK FOR TOkEN AND IF PRESENT, SAVE TO LOCALSTORAGE
+                    console.log(err);
+                }
             })}>
                 <p className="login_email_label" htmlFor="email">Email</p>
-                <input className="login_email_input" {...register("email", {required: true})}/>
+                <input className="login_email_input"
+                       {...register("email",
+                           {
+                               required: true,
+                           })
+                }/>
+                {errors.email && <p className='required_message'>This field is required</p>}
+                
 
                 <p className="login_password_label" htmlFor="password">Password</p>
-                <input className="login_password_input" {...register("password", {required: true})}/>
+                <input className="login_password_input" type='password' 
+                        {...register("password",
+                            {
+                                required: true
+                            })
+                }/>
+                {errors.password && <p className='required_message'>This field is required</p>}
 
                 <button className="login_submit_btn btn" type="submit">Login</button>
             </form>
 
             <p className="signup_here_txt" onClick={() => {
                 registerModalContext.toggleShowRegisterModal(true);
-            }}>Need an account? <span className="signup_btn btn">SIGN UP!</span></p>
-            {
-                registerModalContext.showRegisterModal
-                &&
-                <RegisterModal/>
-            }
-        </div>
-    );
+                }}>Need an account? 
+                <span className="signup_btn btn">SIGN UP!</span>
+            </p>
+            {registerModalContext.showRegisterModal && <RegisterModal/>}
+        </div>);
 }
 
 export default LoginPage;
