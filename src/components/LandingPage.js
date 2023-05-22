6a4e5b6039bd1ab7a05ef756b11fc838cc247b7e
George Puryear <gm.puryear@yahoo.com>
2023-05-21 20:44:51 -0500

    removed the 'useRef' practice

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,26 +1,11 @@
 import LoginPage from './Login';
 import RegisterModalContext from './RegisterModalContext';
 import RegisterModal from './RegisterModal';
-import {useContext, useEffect, useState, useRef} from "react";
+import {useContext} from "react";
 
 const LandingPage = () => {
-    const [name, setName] = useState('');
-    const inputRef = useRef();
-    const prevName = useRef('');
     const registerModalContext = useContext(RegisterModalContext);
-
-    useEffect(() => {
-        prevName.current = name;
-    }, [name]);
-
     
-
-
-    const focus = () => {
-        console.log(inputRef.current.value);
-    }
-
-
     return (
         <>
             {!registerModalContext.showRegisterModal
@@ -29,9 +14,6 @@ const LandingPage = () => {
                 :
                 <RegisterModal/>
             }
-            <input ref={inputRef} value={name} onChange= {e => setName(e.target.value)}/>
-            <p>My names is {name} and it used to be {prevName.current}</p>
-            <button onClick={focus}>Focus</button>
         </>
     )
 }
